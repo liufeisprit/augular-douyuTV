2d015c949dc2391d2201790f37181a37184745b1
liufei <649912343@qq.com>
2017-03-04 09:14:44 +0800

    shouye

--- component/home/home.js
+++ component/home/home.js
@@ -6,6 +6,77 @@ angular.module('homeModule',['ngRoute'])
 		css:'component/home/home.css'
 	});
 }])
-.controller('homeCtrl',['$scope',function($scope){
+.service("catearr",["$http",function($http){
+	this.get=function(){
+		return $http.get("https://m.douyu.com/category")
+		
+	}
+}])
+.service("homeDataArr",["$http",function($http){
+	this.get=function(){
+		return $http.get("https://m.douyu.com/index/getHomeData")
+		
+	}
+}])
+.service("swiper",["$timeout",function($timeout){
+	this.swiper=function(){
+		$timeout(function(){
+			new Swiper('.swiper-container', {
+            loop: true,
+            autoplay:3000,
+            pagination: '.swiper-pagination',
+            paginationClickable :true,
+            observer:true,
+            observeParents:true,
+            loopAdditionalSlides:0,
+            autoplayDisableOnInteraction: false
+      });
+		},50);
+	}
+}])
+.controller('homeCtrl',['$scope',"catearr","$rootScope","homeDataArr","swiper",function($scope,catearr,$rootScope,homeDataArr,swiper){
+	var hot=0;
+	catearr.get().success(function(res){
+		$scope.catearr=res.cate1Info;
+		$scope.catedetailarr=res.cate2Info;
+		$scope.changeall=function(){
+		$scope.catedetailarr=res.cate2Info;
+	}
+	})
+	homeDataArr.get().success(function(res){
+		console.log(res);
+		$scope.slideArr=res.banner;
+		$scope.hotListArr=res.hotList[hot].data;
+		$scope.changehot=function(){
+			hot++;
+			if(hot>3){
+				hot=0
+			}
+			$scope.hotListArr=res.hotList[hot].data;
+		}
+		$scope.livecount=res.liveCount;
+		$scope.liveListArr=res.liveList;
+		$scope.mixListArr=res.mixList;
+		$scope.yzListArr=res.yzList;
+	})
+	swiper.swiper();
+	$scope.changecatetab=function(obj){
+			$.getJSON("https://m.douyu.com/category?type="+obj,function(res){
+				$scope.catedetailarr=res.cate2Info;
+			})
+			$(".nav-header span").bind("touchstart",function(){
+				$(".nav-header span").removeClass("cur");$(this).addClass("cur")
+	})
+		}
+	
+	$scope.getroomList=function(name){
+		$rootScope.roomName=name;
+	}
+	$scope.getroomList2=function(name){
+		$rootScope.roomSlideId=name;
+	}
+	$(".classes").bind("touchstart",function(){
+		$(".layout-nav").slideToggle();
+	})
 	
 }])
