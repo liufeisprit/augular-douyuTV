4ec4e8623cc255e4d6a582e284469b1428a30d79
dzj <414603173@qq.com>
2017-03-06 16:40:19 +0800

    '直接在ctrl里获取数据'

--- component/room/room.js
+++ component/room/room.js
@@ -6,26 +6,12 @@ angular.module('roomModule',['ngRoute'])
 		css:'component/room/room.css'
 	});
 }])
-.service("roomInfo",["$http",'$rootScope',function($http,$rootScope){
-	$rootScope.roomid=JSON.parse(localStorage.getItem('live')).roomid;
-	this.get=function(){
-		return $http.get("https://m.douyu.com/html5/live?roomId="+$rootScope.roomid);
-		
-	}
-}])
-.service("cateInfo",["$http",'$rootScope',function($http,$rootScope){
-	$rootScope.cateid=JSON.parse(localStorage.getItem('live')).cateid;
-	this.get=function(){
-		return $http.get("http://api.douyutv.com/api/v1/live/"+$rootScope.cateid);
-	}
-}])
-.service("hot",["$http",function($http){
-	this.get=function(){
-		return $http.get("https://m.douyu.com/index/getHomeData")
-	}
-}])
-.controller('roomCtrl',['$scope','$rootScope','roomInfo','homeDataArr','cateInfo','$sce',function($scope,$rootScope,roomInfo,hot,cateInfo,$sce){
-	roomInfo.get().success(function(res){
+
+.controller('roomCtrl',['$scope','$rootScope','$http','$sce',function($scope,$rootScope,$http,$sce){
+	var roomid=JSON.parse(localStorage.getItem('live')).roomid;
+	var  cateid=JSON.parse(localStorage.getItem('live')).cateid;
+	
+	$http.get("https://m.douyu.com/html5/live?roomId="+roomid).success(function(res){
 		var v=document.querySelector('video');
 		$scope.roomDetails=res.data;
 			$scope.roomDetails.hls_url=$sce.trustAsResourceUrl($scope.roomDetails.hls_url);
@@ -36,7 +22,7 @@ angular.module('roomModule',['ngRoute'])
 		}
 	});
 	
-	hot.get().success(function(res){
+	$http.get("https://m.douyu.com/index/getHomeData").success(function(res){
 		var hot=0;
 		$scope.hotListArr=res.hotList[hot].data;
 		$scope.changehot=function(){
@@ -48,14 +34,14 @@ angular.module('roomModule',['ngRoute'])
 		}
 	});
 	
-	cateInfo.get().success(function(res){
+	$http.get("http://api.douyutv.com/api/v1/live/"+cateid).success(function(res){
 		$scope.same=res.data;
 	})
 	
 	$scope.changehotlive=function(cateid,roomid){
 		var obj={
-			"cateid":cateid,
-			"roomid":roomid
+			cateid:cateid,
+			roomid:roomid
 		}
 		localStorage.setItem("live",JSON.stringify(obj));
 		location.reload(true);
