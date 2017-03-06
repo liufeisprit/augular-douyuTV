51329064abeb2af1f6b1df27d52f082664b22ab0
dzj <414603173@qq.com>
2017-03-06 11:18:31 +0800

    '视频'

--- component/room/room.js
+++ component/room/room.js
@@ -12,7 +12,17 @@ angular.module('roomModule',['ngRoute'])
 		
 	}
 }])
-.controller('roomCtrl',['$scope','$rootScope','roomInfo','$sce',function($scope,$rootScope,roomInfo,$sce){
+.service("cateInfo",["$http",'$rootScope',function($http,$rootScope){
+	this.get=function(){
+		return $http.get("http://api.douyutv.com/api/v1/live/"+$rootScope.cateid);
+	}
+}])
+.service("hot",["$http",function($http){
+	this.get=function(){
+		return $http.get("https://m.douyu.com/index/getHomeData")
+	}
+}])
+.controller('roomCtrl',['$scope','$rootScope','roomInfo','homeDataArr','cateInfo','$sce',function($scope,$rootScope,roomInfo,hot,cateInfo,$sce){
 	roomInfo.get().success(function(res){
 		var v=document.querySelector('video');
 		$scope.roomDetails=res.data;
@@ -22,5 +32,28 @@ angular.module('roomModule',['ngRoute'])
 			$('#video-poster').hide()
 			v.paused?v.play():v.pause();
 		}
+	});
+	
+	hot.get().success(function(res){
+		var hot=0;
+		$scope.hotListArr=res.hotList[hot].data;
+		$scope.changehot=function(){
+			hot++;
+			if(hot>3){
+				hot=0
+			}
+			$scope.hotListArr=res.hotList[hot].data;
+		}
+	});
+	
+	cateInfo.get().success(function(res){
+		$scope.same=res.data;
 	})
+	
+	$scope.changehotlive=function(cateid,roomid){
+		$rootScope.cateid=cateid;
+		$rootScope.roomid=roomid;
+		console.log($rootScope.cateid);
+		console.log($rootScope.roomid);
+	}
 }])
