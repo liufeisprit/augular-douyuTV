5fb3d99ff8a7bfb586e259fc3229c409862bd53e
dzj <414603173@qq.com>
2017-03-07 09:00:43 +0800

    '最终'

--- component/room/room.js
+++ component/room/room.js
@@ -8,9 +8,10 @@ angular.module('roomModule',['ngRoute'])
 }])
 
 .controller('roomCtrl',['$scope','$rootScope','$http','$sce',function($scope,$rootScope,$http,$sce){
+	//取出存储的房间ID和类别ID
 	var roomid=JSON.parse(localStorage.getItem('live')).roomid;
 	var  cateid=JSON.parse(localStorage.getItem('live')).cateid;
-	
+	//房间信息
 	$http.get("https://m.douyu.com/html5/live?roomId="+roomid).success(function(res){
 		var v=document.querySelector('video');
 		$scope.roomDetails=res.data;
@@ -21,7 +22,7 @@ angular.module('roomModule',['ngRoute'])
 			v.paused?v.play():v.pause();
 		}
 	});
-	
+	//热门直播
 	$http.get("https://m.douyu.com/index/getHomeData").success(function(res){
 		var hot=0;
 		$scope.hotListArr=res.hotList[hot].data;
@@ -34,10 +35,11 @@ angular.module('roomModule',['ngRoute'])
 		}
 	});
 	
+	//同类别直播列表
 	$http.get("http://api.douyutv.com/api/v1/live/"+cateid).success(function(res){
 		$scope.same=res.data;
 	})
-	
+	//存储并刷新页面
 	$scope.changehotlive=function(cateid,roomid){
 		var obj={
 			cateid:cateid,
